55396e07f4c897a9304dea4f9b30a9284383879f
Onur Cem Celebi <onur_cem.celebi@baloise.com>
2015-10-23 16:52:05 +0200

    Clean up

--- tests/init.pp
+++ tests/init.pp
@@ -1,12 +1,1 @@
-#include '::fooacl'
-
-fooacl::conf { 'foo':
-  target => [
-    '/var/test/a.a',
-    '/var/test/b.b',
-  ],
-  permissions => [
-    'user:sssd:rwX',
-    'group:puppet:rwX',
-  ],
-}
+include '::fooacl'
